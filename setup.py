cd942b7176dc04671e1a7825a968555a84288cfa
Jesse Dhillon <jesse@deva0.net>
2014-09-28 23:06:55 -0700

    Adds `psyopg2`, `geoalchemy` dependencies
    Also, PEP8

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import os
 
 from setuptools import setup, find_packages
 
-here =      os.path.abspath(os.path.dirname(__file__))
-readme =    open(os.path.join(here, 'README.rst')).read()
-changes =   open(os.path.join(here, 'CHANGES.rst')).read()
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, 'README.rst')).read()
+changes = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = [
-    'MySQL-python',
+    'psycopg2',
     'pyramid',
     'SQLAlchemy',
     'transaction',
@@ -21,13 +21,14 @@ requires = [
     'python-dateutil<2.0',
     'sqlalchemy-batteries',
     'docopt',
+    'geoalchemy',
 ]
 
 setup(
     name='bonanza',
     version='0.0',
     description='bonanza',
-    long_description="{readme}\n\n{changes}".format(readme=readme, changes=changes),
+    long_description="{}\n\n{}".format(readme, changes),
     classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
@@ -42,8 +43,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     test_suite='bonanza',
-    install_requires = requires,
-    entry_points = {
+    install_requires=requires,
+    entry_points={
         'paste.app_factory': [
             'main = bonanza:main',
         ],
