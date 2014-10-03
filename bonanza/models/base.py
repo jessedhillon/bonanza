af799ef58c660034ca9a39cc2cd68b326bb026d3
Jesse Dhillon <jesse@deva0.net>
2014-10-02 23:47:49 -0700

    Adds `CraigslistListing` model

--- bonanza/models/base.py
+++ bonanza/models/base.py
@@ -5,15 +5,26 @@ from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from sqlalchemy.schema import ForeignKey, Column, Index, UniqueConstraint, ForeignKeyConstraint
 from sqlalchemy.sql import and_, or_, not_, null
 from sqlalchemy.sql.expression import desc, asc
-from sqlalchemy.types import *
+from sqlalchemy.types import Unicode, UnicodeText, Integer, Numeric, Date
 from geoalchemy2 import Geometry
 from batteries.model import Model
-from batteries.model.hashable import Hashable, HashableKey, HashableReference
+from batteries.model.hashable import Hashable, HashableKey
 from batteries.model.recordable import Recordable
-from batteries.model.types import UTCDateTime, UUID
+
+from bonanza.lib.util import geoproperty
 
 
-class Listing(Hashable, Recordable, Model):
+class CraigslistListing(Hashable, Recordable, Model):
     _key = HashableKey()
 
-    location = Column(Geometry('POINT'))
+    id = Column(Unicode(20), nullable=False)
+    title = Column(UnicodeText, nullable=False)
+    url = Column(Unicode(200), nullable=False)
+    image_thumbnail_url = Column(Unicode(300))
+
+    bedrooms = Column(Integer)
+    posted_date = Column(Date, nullable=False)
+    ask = Column(Numeric(12, scale=2), nullable=False)
+
+    _location = Column('location', Geometry('POINT'))
+    location = geoproperty('_location', 'point')
