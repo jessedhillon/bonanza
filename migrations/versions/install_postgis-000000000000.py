30ab154f08a8a0a893cfc7054c04754d2702013a
Jesse Dhillon <jesse@deva0.net>
2014-10-29 17:44:32 +0000

    Minor fix to recent migrations

--- migrations/versions/install_postgis-000000000000.py
+++ migrations/versions/install_postgis-000000000000.py
@@ -1,4 +1,4 @@
-"""initial schema
+"""install postgis extensions
 
 Revision ID: 000000000000
 Revises: None
@@ -29,7 +29,7 @@ def upgrade():
         op.execute("alter schema tiger owner to rds_superuser")
         op.execute("alter schema topology owner to rds_superuser")
         op.execute("""
-CREATE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
+CREATE OR REPLACE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
 SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO rds_superuser')
   FROM (
     SELECT nspname, relname
