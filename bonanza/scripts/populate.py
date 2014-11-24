b2f109a11accebe5812a4e3b4169c98ae16a1205
Jesse Dhillon <jesse@deva0.net>
2014-11-24 14:37:58 -0800

    WIP

--- bonanza/scripts/populate.py
+++ bonanza/scripts/populate.py
@@ -5,16 +5,19 @@ Usage:
     populate_bonanza (-h | --help)
 
 Options:
-    -c <config>, --config=<config>      The path to the configuration file, [default: ./bonanza.ini]
+    -c <config>, --config=<config>      The path to the configuration file,
+                                        [default: ./bonanza.ini]
 """
 
 import sys
+import logging
 
 from docopt import docopt
 
 import bonanza.models as models
 import bonanza.scripts.common as common
-from bonanza.models.base import Concept, Feature, Dimension, Segment
+from bonanza.models.base import Concept, Feature, Dimension, Segment, CensusBlock,\
+    CensusBlockSegment
 
 logger = None
 
@@ -22,12 +25,15 @@ logger = None
 
 
 def main(argv=sys.argv):
+    global logger
+
     try:
         settings = None
         arguments = docopt(__doc__)
         config_uri = arguments['--config']
         settings = common.configure(config_uri)
 
+        logger = logging.getLogger(__name__)
         populate(arguments, settings)
 
     except Exception:
@@ -53,8 +59,45 @@ def populate(arguments, settings):
     }]
 
     dimensions = [{
+        'id': u'census_block',
+        'name': u"Census Block",
+        'segments': []
+    }, {
         'id': u'bedrooms',
         'name': u"Number of Bedrooms",
+        'segments': [{
+            'id': 'all',
+            'name': 'Any',
+            'sort_value': 0,
+        }, {
+            'id': '1',
+            'name': '1',
+            'value': 1,
+            'sort_value': 1,
+        }, {
+            'id': '2',
+            'name': '2',
+            'value': 2,
+            'sort_value': 2,
+        }, {
+            'id': '3',
+            'name': '3',
+            'value': 3,
+            'sort_value': 3,
+        }, {
+            'id': '4',
+            'name': '4',
+            'value': 4,
+            'sort_value': 4,
+        }, {
+            'id': '5+',
+            'name': '5 or more',
+            'value': '5+',
+            'sort_value': 5,
+        }]
+    }, {
+        'id': u'bathrooms',
+        'name': u"Number of Bathrooms",
         'segments': [{
             'id': 'all',
             'name': 'Any',
@@ -91,19 +134,24 @@ def populate(arguments, settings):
         if Concept.get(c['id']):
             continue
 
+        logger.info("inserting concept {id}".format(**c))
         models.session.add(Concept(**c))
 
     for f in features:
         if Feature.get(f['id']):
             continue
 
+        logger.info("inserting feature {id}".format(**f))
         models.session.add(Feature(**f))
 
     for d in dimensions:
         segments = d.pop('segments', [])
         dimension = Dimension.get(d['id']) or Dimension(**d)
+        logger.info("inserting dimension {id}".format(**d))
+
         for s in segments:
             segment = Segment.get(d['id'], s['id']) or Segment(**s)
+            logger.info("inserting segment {}/{}".format(d['id'], s['id']))
             dimension.segments.append(segment)
 
         models.session.add(dimension)
